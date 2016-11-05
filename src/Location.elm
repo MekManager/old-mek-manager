68028bd2b4c549b9d4082a62a50bc531163b0c93
Eric Hopkins <eric.on.tech@gmail.com>
2016-11-05 19:18:47 -0400

    Add components and locations

---
+++ src/Location.elm
@@ -0,0 +1,99 @@
+module Location exposing (..)
+
+import Component exposing (..)
+
+
+type Location = SixSlot | TwelveSlot
+
+type alias SixSlot =
+    { first : Maybe Component
+    , second : Maybe Component
+    , third : Maybe Component
+    , fourth : Maybe Component
+    , fifth : Maybe Component
+    , sixth : Maybe Component
+    }
+
+type alias TwelveSlot =
+    { top : SixSlot
+    , bottom : SixSlot
+    }
+
+{-| Factory function returning a default arm
+-}
+arm : TwelveSlot
+arm =
+    TwelveSlot
+        SixSlot
+            Just Shoulder
+            Just UpperArmActuator
+            Just LowerArmActuator
+            Just HandActuator
+            Nothing
+            Nothing
+        empty
+
+{-| Factory function returning a default center torso
+-}
+centerTorso : TwelveSlot
+centerTorso =
+    TwelveSlot
+        SixSlot
+            Just Engine
+            Just Engine
+            Just Engine
+            Just Gyro
+            Just Gyro
+            Just Gyro
+        SixSlot
+            Just Gyro
+            Just Engine
+            Just Engine
+            Just Engine
+            Nothing
+            Nothing
+
+{-| Factory function returning a default head
+-}
+head : SixSlot
+head =
+    SixSlot
+        Just LifeSupport
+        Just Sensors
+        Just Cockpit
+        Nothing
+        Just Sensors
+        Just LifeSupport
+
+{-| Factory function returning a default leg
+-}
+leg : SixSlot
+leg =
+    SixSlot
+        Just Hip
+        Just UpperLegActuator
+        Just LowerLegActuator
+        Just FootActuator
+        Nothing
+        Nothing
+
+{-| Factory function returning a default side torso
+-}
+sideTorso : TwelveSlot
+sideTorso =
+    TwelveSlot
+        empty
+        empty
+
+-- UNEXPOSED --
+{-| Factory function returning an empty SixSlot
+-}
+empty : SixSlot
+empty =
+    SixSlot
+        Nothing
+        Nothing
+        Nothing
+        Nothing
+        Nothing
+        Nothing
