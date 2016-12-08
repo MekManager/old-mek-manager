f7fddeef6349e48917fa763ce4e3b0567124363d
Eric Hopkins <eric.on.tech@gmail.com>
2016-12-08 12:28:38 -0500

    Reduce number of imports from Html in Main.elm

--- src/Main.elm
+++ src/Main.elm
@@ -1,67 +1,53 @@
-import Html exposing ( Html
-                     , b
-                     , div
-                     , fieldset
-                     , h1
-                     , h3
-                     , option
-                     , p
-                     , select
-                     , button
-                     , label
-                     , br
-                     , text
-                     , input
-                     )
+import Html as H exposing (Html, text)
+import Html.Attributes as A
+import Html.Events as E
 import Mek exposing (Mek)
 import TechBase exposing (TechBase)
-import Html.Attributes exposing (style, type_, name, checked)
-import Html.Events exposing (onClick, onInput)
 
 main : Program Never Mek Msg
 main =
-    Html.beginnerProgram { model = Mek.factory
-                         , view = view
-                         , update = update
-                         }
+    H.beginnerProgram { model = Mek.factory
+                      , view = view
+                      , update = update
+                      }
 
 view : Mek -> Html Msg
 view model =
-    div []
-        [ h1 [] [ text "Mek Designer" ]
-        , h3 [ style [ ("margin", "0") ] ] [ text "Debug info:" ]
+    H.div []
+        [ H.h1 [] [ text "Mek Designer" ]
+        , H.h3 [ A.style [ ("margin", "0") ] ] [ text "Debug info:" ]
         , debugInfo "Techbase: " (TechBase.toString model.techBase)
         , debugInfo "Omnimech: " (toString model.omniMech)
         , debugInfo "Weight: " (toString model.weight)
-        , br [] []
-        , b [] [ text "Select Tech Base: " ]
-        , select [ onInput ChangeTechBase ]
-                 [ option [] [ text "Inner Sphere" ]
-                 , option [] [ text "Clan" ]
+        , H.br [] []
+        , H.b [] [ text "Select Tech Base: " ]
+        , H.select [ E.onInput ChangeTechBase ]
+                 [ H.option [] [ text "Inner Sphere" ]
+                 , H.option [] [ text "Clan" ]
                  ]
-        , br [] []
-        , b [] [ text "Change Omni Status: " ]
-        , fieldset []
-                   [ label []
-                           [ input [ type_ "radio", name "omni", checked False, onClick (ChangeOmni True) ] []
-                           , text "True"
-                           ]
-                   , label []
-                           [ input [ type_ "radio", name "omni", checked True, onClick (ChangeOmni False) ] []
-                           , text "False"
-                           ]
-                   ]
-        , br [] []
-        , b [] [ text "Change Weight: "]
-        , button [ onClick IncreaseWeight ] [ text "Increase" ]
-        , button [ onClick DecreaseWeight ] [ text "Decrease" ]
+        , H.br [] []
+        , H.b [] [ text "Change Omni Status: " ]
+        , H.fieldset []
+                     [ H.label []
+                               [ H.input [ A.type_ "radio", A.name "omni", A.checked False, E.onClick (ChangeOmni True) ] []
+                               , text "True"
+                               ]
+                     , H.label []
+                               [ H.input [ A.type_ "radio", A.name "omni", A.checked True, E.onClick (ChangeOmni False) ] []
+                               , text "False"
+                               ]
+                     ]
+        , H.br [] []
+        , H.b [] [ text "Change Weight: " ]
+        , H.button [ E.onClick IncreaseWeight ] [ text "Increase" ]
+        , H.button [ E.onClick DecreaseWeight ] [ text "Decrease" ]
         ]
 
 debugInfo : String -> String -> Html Msg
 debugInfo description value =
-    div []
-        [ b [] [ text description ]
-        , p [ style [ ("display", "inline-block")
+    H.div []
+        [ H.b [] [ text description ]
+        , H.p [ A.style [ ("display", "inline-block")
                     , ("margin", "0")
                     ]
             ]
